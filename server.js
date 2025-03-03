84a73edd69586556d4389393877d516ab1e1f931
Cesar Guanipa <cesar10guanipa@gmail.com>
2025-03-03 18:12:14 -0300

    added enviroment variable for express port

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const dotenv = require("dotenv");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
